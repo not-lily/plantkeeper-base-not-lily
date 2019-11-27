663e5adc50e8a886c04f4dd3566bb28299b4849a
lily <57730841+not-lily@users.noreply.github.com>
2019-11-27 12:30:38 -0800

    Add files via upload

---
+++ PlantKeeper/LightSensor.cpp
@@ -0,0 +1,54 @@
+#include "LightSensor.h"
+
+const int LightError = -1000;
+
+// Function declarations
+
+// Name: LightSetup
+// Parameters: int SensorPin - the pin the sensor is attached to
+// Returns: True if the setup succeeds, false if it fails
+bool LightSetup(int SensorPin)
+
+{
+  Serial.println("Light sensor setup not yet implemented");
+
+  return false;
+}
+
+int LightLoop(int SensorPin)
+{
+const int sensorMin = 0;
+// sensor maximum, discovered through experiment
+const int sensorMax = 800;
+
+int photocellPin = A5;
+  int analogValue;
+  int range;
+
+    analogValue = analogRead(photocellPin);
+    range = map(analogValue, sensorMin, sensorMax, 0, 3);
+
+    switch (range) 
+    {
+        // your hand is on the sensor
+        case 0:
+            Serial.print("Light: ");
+            Serial.println("dark");
+            break;
+        // your hand is close to the sensor
+        case 1:
+            Serial.print("Light: ");
+            Serial.println("dim");
+            break;
+        // your hand is a few inches from the sensor
+        case 2: 
+            Serial.print("Light: ");
+            Serial.println("medium");
+            break;
+        // your hand is nowhere near the sensor
+        case 3:
+            Serial.print("Light: ");
+            Serial.println("bright");
+            break;
+    }
+}
---
+++ PlantKeeper/LightSensor.h
@@ -0,0 +1,15 @@
+
+// LightSensor.h
+#ifndef LIGHT_SENSOR_H
+#define LIGHT_SENSOR_H
+
+// Include all the standard Arduino libraries
+#include <arduino.h>
+
+// Function prototypes
+bool LightSetup(int SensorPin);
+int LightLoop(int SensorPin);
+
+
+
+#endif // LIGHT_SENSOR_H
